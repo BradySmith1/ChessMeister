cd3463f13bab06a9d79203c460dbc1acd1064ef4
Brady Smith <bksmith8@catamount.wcu.edu>
2023-03-22 17:26:29 -0400

    Moved the driver file to a better place.

--- ChessMiester/src/controller/Driver.java
+++ ChessMiester/src/controller/Driver.java
@@ -1,7 +1,14 @@
 package controller;
 
+import model.Board;
+import uicli.BoardColorCLI;
+import uicli.BoardMonoCLI;
+
 public class Driver {
     public static void main(String[] args){
-
+        Board board = new Board(new BoardMonoCLI());
+        board.initBoard();
+        board.setup();
+        board.draw();
     }
 }
