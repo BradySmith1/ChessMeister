26cbaa51ab2b76172a52186593f9bff68c57cae1
Brady Smith <bksmith8@catamount.wcu.edu>
2023-03-26 17:20:28 -0400

    Reformatted the main menu to translate to our uml diagram. also created the functionality for a new game and end game.

--- ChessMiester/src/controller/Chess.java
+++ ChessMiester/src/controller/Chess.java
@@ -5,25 +5,111 @@ import enums.Rank;
 import interfaces.BoardIF;
 import model.Board;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Chess {
 
-    private BoardIF boardGame;
-    private static final int DEFAULT_GAME = 1;
+    private BoardIF board;
+
+    private Scanner scan;
+
+    //private Player player1;
+
+    //private Player player2;
+
 
     public Chess() {
-        this(DEFAULT_GAME);
+        scan = new Scanner(System.in); /* scanner to read in player choice */
     }
 
-    public Chess(int gameType) {
-        System.out.println("Feature not yet finished");
+    public void mainMenu() {
+        System.out.println("   _____ _                   __  __      _     _            \n" +
+                "  / ____| |                 |  \\/  |    (_)   | |           \n" +
+                " | |    | |__   ___  ___ ___| \\  / | ___ _ ___| |_ ___ _ __ \n" +
+                " | |    | '_ \\ / _ \\/ __/ __| |\\/| |/ _ \\ / __| __/ _ \\ '__|\n" +
+                " | |____| | | |  __/\\__ \\__ \\ |  | |  __/ \\__ \\ ||  __/ |   \n" +
+                "  \\_____|_| |_|\\___||___/___/_|  |_|\\___|_|___/\\__\\___|_|   \n");
+        String menu =
+                "---------------------------------------------------------------\n" +
+                        "Please make a selection as to what you would like to do:\n" +
+                        "1 - Play Local Game Against another Player\n" +
+                        "(COMING SOON!) 2 - Play Local Game Against a Computer\n" +
+                        "(COMING SOON!) 3 - Play Online Game Against another Player\n" +
+                        "(COMING SOON!) 4 - 4-Player Chess!\n" +
+                        "0 - Exit Game\n--------------------------------------------------------------\n";
+        String prompt = "Enter your menu choice here -> ";
+        int choice = 999;     //initialized to 5 so there is no option chosen or quitting the loop
+        while (choice != 0) {   //while user has not quit
+            System.out.println(menu);   //shows user menu options
+            System.out.print(prompt);
+            try {
+                choice = scan.nextInt();
+            }
+            catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a number (0 - 4)");
+            }
+            System.out.println();
+            switch(choice) {
+                case 1:
+                    newGame();
+                    break;
+                case 2:
+                    System.out.println("This feature is coming soon!");
+                    break;
+                case 3:
+                    System.out.println("This feature is coming soon!");
+                    break;
+                case 4:
+                    System.out.println("This feature is coming soon!");
+                    break;
+                case 0:
+                    endGame();
+                    break;
+                default:
+                    System.out.println("Please enter a valid number (0 - 4)");
+                    break;
+            }
+            System.out.println();
+            scan.nextLine();    //consumes a new line so that nextInt throwing an exception will not loop
+        }
     }
+
 
     public void newGame() {
-        
+        System.out.println("Player one color: (1) White or (2) Black");
+        int color = scan.nextInt();
+        if (color == 1) {
+            //player1 = new Player(GameColor.WHITE);
+            //player2 = new Player(GameColor.BLACK);
+        }
+        else if (color == 2) {
+            //player1 = new Player(GameColor.BLACK);
+            //player2 = new Player(GameColor.WHITE);
+        }
+        else {
+            System.out.println("Invalid input. Defaulting to white.");
+            //player1 = new Player(GameColor.WHITE);
+            //player2 = new Player(GameColor.BLACK);
+        }
+        //clears the screen
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        board = new Board();
+        board.setup();
+        board.draw();
+        //loop for move logic.
+        while(true){
+            endGame();
+        }
     }
 
     public void endGame() {
-
+        System.out.println("The game should be over now.\nreturning to main menu.");
+        //clears the screen
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        this.mainMenu();
     }
 
     public BoardIF loadGame(String file) {
@@ -31,10 +117,6 @@ public class Chess {
     }
 
     public void saveGame(String file, BoardIF game) {
-
-    }
-
-    public void move(Files fromF, Rank fromR, Files toF, Rank toR) {
 
     }
 }
--- ChessMiester/src/controller/Driver.java
+++ ChessMiester/src/controller/Driver.java
@@ -15,41 +15,7 @@ import uicli.BoardMonoCLI;
 
 public class Driver {
     public static void main(String[] args){
-        Scanner scan = new Scanner(System.in); /* scanner to read in player choice */
-        System.out.println("   _____ _                   __  __      _     _            \n" +
-                           "  / ____| |                 |  \\/  |    (_)   | |           \n" +
-                           " | |    | |__   ___  ___ ___| \\  / | ___ _ ___| |_ ___ _ __ \n" +
-                           " | |    | '_ \\ / _ \\/ __/ __| |\\/| |/ _ \\ / __| __/ _ \\ '__|\n" +
-                           " | |____| | | |  __/\\__ \\__ \\ |  | |  __/ \\__ \\ ||  __/ |   \n" +
-                           "  \\_____|_| |_|\\___||___/___/_|  |_|\\___|_|___/\\__\\___|_|   \n");
-        String menu =
-                "Welcome to ChessMeister!\n---------------------------------------------------------------\n" +
-                "Please make a selection as to what you would like to do:\n" +
-                "1 - Play Local Game Against another Player\n" +
-                "(COMING SOON!) 2 - Play Local Game Against a Computer\n" +
-                "(COMING SOON!) 3 - Play Online Game Against another Player\n" +
-                "(COMING SOON!) 4 - 4-Player Chess!\n" +
-                "0 - Exit Game\n--------------------------------------------------------------\n";
-        String prompt = "Enter your menu choice here -> ";
-        int choice = 5;     //initialized to 5 so there is no option chosen or quitting the loop
-        Chess chess;    //initialization of the Chess class
-        while (choice != 0) {   //while user has not quit
-            System.out.println(menu);   //shows user menu options
-            System.out.print(prompt);
-            try {
-                choice = scan.nextInt();
-            }
-            catch (InputMismatchException e) {
-                System.out.println("\n" + e);
-            }
-            System.out.println();
-            if (choice >= 1 && choice <= 4) {
-                chess = new Chess(choice);  //new game of chess based on the game type
-            } else if (choice != 0) {
-                System.out.println("Please enter a valid number (0 - 4)");
-            }
-            System.out.println();
-            scan.nextLine();    //consumes a new line so that nextInt throwing an exception will not loop
-        }
+        Chess chess = new Chess();
+        chess.mainMenu();
     }
 }
