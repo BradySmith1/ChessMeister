8ebfe662727b95731e33c8606175628febe463bb
zteanes <zteanes1@catamount.wcu.edu>
2023-05-02 11:06:43 -0400

    copied in colorama files from scott

---
+++ ChessMiester/src/gui/colorama/VolumeControl.java
@@ -0,0 +1,9 @@
+//package gui.colorama;
+//
+//public class VolumeControl extends SliderPane {
+//
+//	public VolumeControl(int initVal) {
+//		super("volume", initVal, 0, 100);
+//	}//emd volume control
+//
+//}//end volume control
