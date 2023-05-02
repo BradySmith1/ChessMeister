150d02c4c9be1a6dedcf12eb3f669f44fddaadac
zteanes <zteanes1@catamount.wcu.edu>
2023-05-02 13:02:36 -0400

    main menu gui began

---
+++ ChessMiester/src/gui/MainMenu/MainMenuGUI.java
@@ -0,0 +1,161 @@
+/**
+ * This class is the GUI implementation for the main menu.
+ *
+ * @author Zach Eanes (50%), Kaushal Patel (50%)
+ */
+package gui.MainMenu;
+
+import interfaces.MainMenuIF;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class MainMenuGUI extends BorderPane {
+
+    /** The main menu pane. */
+    BorderPane mainMenuPane;
+
+    /** Scene */
+    private Scene scene;
+
+    /** Label */
+    private Label topLabel;
+
+    private static MainMenuGUI instance;
+
+    /**
+     * Constructor for the main menu GUI.
+     */
+    public static MainMenuGUI getInstance(){
+        if(instance == null){
+            instance = new MainMenuGUI();
+        }
+        return instance;
+    }
+
+
+    private MainMenuGUI(){
+        // Create a border pane
+        this.mainMenuPane = new BorderPane();
+
+        // set parts of the pane
+        Label top = makeTop();
+        this.mainMenuPane.setTop(top);
+
+        VBox center = makeCenter();
+        this.mainMenuPane.setCenter(center);
+
+        ImageView left = makeLeft();
+        this.mainMenuPane.setLeft(left);
+
+        ImageView right = makeRight();
+        this.mainMenuPane.setRight(right);
+
+        AnchorPane bottom = makeBottom();
+        this.mainMenuPane.setBottom(bottom);
+
+        // set center alignments
+        BorderPane.setAlignment(top, Pos.CENTER);
+        BorderPane.setAlignment(center, Pos.CENTER);
+        BorderPane.setAlignment(left, Pos.CENTER);
+        BorderPane.setAlignment(right,Pos.CENTER);
+
+        // Create a scene object
+        this.scene = new Scene(mainMenuPane);
+
+        // Get stylesheet
+        this.scene.getStylesheets().add(
+                   getClass().getResource("MainMenu.css").toExternalForm());
+    }
+    /**
+     * Getter for the scene.
+     * @return the scene
+     */
+    public Scene getMenu(){ return this.scene; }
+
+    /**
+     * Method that creates the top component of the main menu
+     */
+    private Label makeTop(){
+        topLabel = new Label("Chess Meister");
+        topLabel.setId("welcome-text");
+        return topLabel;
+    }
+
+    private ImageView makeLeft(){
+        ImageView imageView = new ImageView();
+        imageView.setImage(new Image("gui/MainMenu/img/KingPiece.png"));
+        imageView.setFitHeight(400);
+        imageView.setFitWidth(400);
+        imageView.setPreserveRatio(true);
+        return imageView;
+    }
+
+    private ImageView makeRight(){
+        ImageView imageView = new ImageView();
+        imageView.setImage(new Image("gui/MainMenu/img/KingPiece.png"));
+        imageView.setFitHeight(400);
+        imageView.setFitWidth(400);
+        imageView.setPreserveRatio(true);
+        return imageView;
+    }
+
+    /**
+     * This method is used to create the bottom section of our main menu.
+     */
+    private AnchorPane makeBottom(){
+        //create 2 buttons and set ids
+        Button settings = new Button("Settings");
+        Button exit = new Button("Exit");
+        settings.setId("bottom-button");
+        exit.setId("bottom-button");
+
+        //create anchor pane and add buttons
+        AnchorPane ap = new AnchorPane(settings, exit);
+
+        //anchor the settings button
+        AnchorPane.setBottomAnchor(settings, 10.0);
+        AnchorPane.setLeftAnchor(settings, 10.0);
+
+        //anchor the exit button
+        AnchorPane.setRightAnchor(exit, 10.0);
+        AnchorPane.setRightAnchor(exit, 10.0);
+
+        return ap;
+    }
+
+    /**
+     * Method that creates the center components of the main menu
+     */
+    private VBox makeCenter(){
+        // Create buttons
+        Button versusPlayer = new Button("Play Chess");
+        Button definePlayers = new Button("Define Players");
+        Button loadGame = new Button("Load Game");
+        Button playedGames = new Button("View Played Games");
+        Button tutorial = new Button("View Tutorials");
+
+        // Set button ids
+        versusPlayer.setId("menu-button");
+        definePlayers.setId("menu-button");
+        loadGame.setId("menu-button");
+        playedGames.setId("menu-button");
+        tutorial.setId("menu-button");
+
+        // Create a vbox and add buttons
+        VBox centerBox = new VBox();
+        centerBox.setSpacing(10);
+        centerBox.getChildren().addAll(versusPlayer, definePlayers,
+                                       loadGame, playedGames, tutorial);
+
+        // Center buttons in Vbox
+        centerBox.setAlignment(javafx.geometry.Pos.CENTER);
+        return centerBox;
+    }
+}
---
+++ ChessMiester/src/gui/controller/Main.java
@@ -0,0 +1,41 @@
+/**
+ * This class is the main file used to create our GUI implementation
+ * of chess.
+ *
+ * @author Zach Eanes (50%), Kaushal Patel (50%)
+ */
+package gui.controller;
+
+import gui.MainMenu.MainMenuGUI;
+import javafx.application.Application;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+public class Main extends Application {
+
+    /**
+     * This is our main driver that launches the GUI.
+     *
+     * @param args command line arguments passed in
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * This method is used to start the GUI.
+     *
+     * @param stage the stage to be used
+     */
+    @Override
+    public void start(Stage stage) {
+        MainMenuGUI mainMenu = MainMenuGUI.getInstance();
+        // Create ChessMeister Label
+        stage.setTitle("ChessMeister");
+
+        stage.setScene(mainMenu.getMenu());
+        stage.setMaximized(true);
+        stage.getScene().setFill(Color.GREY);
+        stage.show();
+    }
+}
